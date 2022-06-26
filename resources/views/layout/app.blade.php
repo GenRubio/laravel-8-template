904be4489e41cd0a29c51c0bcfd43f0f6aa78106
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2022-06-26 11:33:09 +0200

    WIP :: Update layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -17,11 +17,6 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @yield('personal-style')
-
-    <script type="text/javascript">
-        @include('web-config');
-    </script>
 </head>
 
 <body>
